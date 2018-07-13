bb980591c79c05be4b0f8aa00e950d81517b5f6c
Vrezh <hovanisyan.vrezh@gmail.com>
2018-07-14 01:57:33 +0400

    [add] solution for all users

--- src/fetch.js
+++ src/fetch.js
@@ -1,18 +1,31 @@
 const fContainer = document.querySelector('.fetch-result');
 
+let listOfUsers = [];
+
 fetch('http://jsonplaceholder.typicode.com/users')
   .then(r => r.json())
-  .then(([{ id }]) => {
-    return fetch(`http://jsonplaceholder.typicode.com/posts?userId=${id}`);
-  })
-  .then(r => r.json())
-  .then(posts => {
-    const list = document.createElement('ul');
-    posts.map(post => {
-      const li = document.createElement('li');
-      li.textContent = post.title;
-      list.appendChild(li);
-    });
-    fContainer.appendChild(list);
-  })
-  .catch(err => alert(`Oops! It seems that we have an error: ${err}`));
+  .then(users =>
+    users.map(user => {
+      listOfUsers.push(user);
+      return fetch(
+        `http://jsonplaceholder.typicode.com/posts?userId=${user.id}`
+      );
+    })
+  )
+  .then(arrOfFetchPromises => Promise.all(arrOfFetchPromises))
+  .then(arrOfFetchResponses => arrOfFetchResponses.map(r => r.json()))
+  .then(arrOfResolvedPromises => Promise.all(arrOfResolvedPromises))
+  .then(arrOfPostsByUser =>
+    arrOfPostsByUser.map((posts, index) => {
+      const header = document.createElement('h2');
+      header.textContent = `${listOfUsers[index].name}'s posts:`;
+      fContainer.appendChild(header);
+      const ul = document.createElement('ul');
+      posts.map(post => {
+        const li = document.createElement('li');
+        li.textContent = post.title;
+        ul.appendChild(li);
+      });
+      fContainer.appendChild(ul);
+    })
+  );
